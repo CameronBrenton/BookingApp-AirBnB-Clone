bc121fcea293b75349ace18a389c482589301335
CameronBrenton <71828277+CameronBrenton@users.noreply.github.com>
2023-06-13 15:51:40 -0230

    Install dotenv and mongoose dependencies

--- api/index.js
+++ api/index.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+require('dotenv').config();
 const app = express();
+
+app.use(express.json());
 
 app.use(cors({
 	credentials: true,
 	origin: 'http://localhost:5173',
 }));
 
+mongoose.connect(process.env.MONGO_URL)
+
 app.get('/test', (req, res) => {
 	res.json('test ok');
 });
 
-app.listen(4000);
+app.post('/register', (req, res) => {
+	const {name, email, password} = req.body;
+	res.json({name, email, password});
+})
+
+app.listen(4000);			
