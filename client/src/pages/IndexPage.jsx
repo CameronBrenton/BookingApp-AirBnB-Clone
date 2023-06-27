6aec381aac87054b50c90767e9a15ed4e6f84938
CameronBrenton <71828277+CameronBrenton@users.noreply.github.com>
2023-06-27 18:22:32 -0230

    First commit
    -Begin creating the grid layout homepage that AirBnB has which displays the user's account places

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,8 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function IndexPage() {
+  const [places, setPlaces] = useState([]);
+  useEffect(() => {
+    axios.get("/places").then((response) => {
+      setPlaces(response.data);
+    });
+  }, []);
   return (
-    <div>
-      Index page here
+    <div className="grid mt-8 grid-cold-2 md:grid-col-3 lg:grid-col-4">
+      {places.length > 0 &&
+        places.map((place) => (
+          <div>
+            <div className="bg-gray-500">
+              {place.photos?.[0] && (
+                <img
+                  src={"http://localhost:4000/uploads/" + place.photos?.[0]}
+                  alt=""
+                />
+              )}
+            </div>
+            {place.title}
+          </div>
+        ))}
     </div>
   );
 }
