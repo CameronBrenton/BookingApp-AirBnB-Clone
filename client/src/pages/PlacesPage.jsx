b714204f34c955f7f7e5107a876c5b089b4d0434
CameronBrenton <71828277+CameronBrenton@users.noreply.github.com>
2023-06-27 13:10:29 -0230

    Allow the PlacesPage to reset it's data once the user clicks a place in their profile

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,32 +1,56 @@
 import { Link } from "react-router-dom";
 import AccountNav from "../AccountNav";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function PlacesPage() {
+  const [places, setPlaces] = useState([]);
+  useEffect(() => {
+    axios.get("/places").then(({ data }) => {
+      setPlaces(data);
+    });
+  }, []);
   return (
     <div>
-		<AccountNav />
-        <div className="text-center">
-          <Link
-            className="inline-flex items-center gap-1 px-6 py-2 text-white rounded-full bg-primary"
-            to={"/account/places/new"}
+      <AccountNav />
+      <div className="text-center">
+        <Link
+          className="inline-flex items-center gap-1 px-6 py-2 text-white rounded-full bg-primary"
+          to={"/account/places/new"}
+        >
+          <svg
+            width="15"
+            height="15"
+            viewBox="0 0 15 15"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            <svg
-              width="15"
-              height="15"
-              viewBox="0 0 15 15"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M8 2.75C8 2.47386 7.77614 2.25 7.5 2.25C7.22386 2.25 7 2.47386 7 2.75V7H2.75C2.47386 7 2.25 7.22386 2.25 7.5C2.25 7.77614 2.47386 8 2.75 8H7V12.25C7 12.5261 7.22386 12.75 7.5 12.75C7.77614 12.75 8 12.5261 8 12.25V8H12.25C12.5261 8 12.75 7.77614 12.75 7.5C12.75 7.22386 12.5261 7 12.25 7H8V2.75Z"
-                fill="currentColor"
-                fillRule="evenodd"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-            Add new place
-          </Link>
-        </div>
+            <path
+              d="M8 2.75C8 2.47386 7.77614 2.25 7.5 2.25C7.22386 2.25 7 2.47386 7 2.75V7H2.75C2.47386 7 2.25 7.22386 2.25 7.5C2.25 7.77614 2.47386 8 2.75 8H7V12.25C7 12.5261 7.22386 12.75 7.5 12.75C7.77614 12.75 8 12.5261 8 12.25V8H12.25C12.5261 8 12.75 7.77614 12.75 7.5C12.75 7.22386 12.5261 7 12.25 7H8V2.75Z"
+              fill="currentColor"
+              fillRule="evenodd"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+          Add new place
+        </Link>
+      </div>
+      <div className="mt-4">
+        {places.length > 0 &&
+          places.map((place, index) => (
+            <Link to={'/account/places/'+place._id} key={index} className="flex gap-4 p-4 bg-gray-100 cursor-pointer rounded-2xl">
+              <div className="w-32 h-32 bg-gray-300 grow shrink-0">
+                {place.photos.length > 0 && (
+                  <img src={place.photos[0]} alt="" />
+                )}
+              </div>
+              <div className="grow-0 shrink">
+                <h2 className="text-xl">{place.title}</h2>
+                <p className="mt-2 text-sm">{place.description}</p>
+              </div>
+            </Link>
+          ))}
+      </div>
     </div>
   );
 }
