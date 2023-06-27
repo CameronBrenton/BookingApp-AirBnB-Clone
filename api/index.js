e446a66798541fc152e6b451586b5f0e4e0ef60f
CameronBrenton <71828277+CameronBrenton@users.noreply.github.com>
2023-06-27 13:05:36 -0230

    Add /places routes and Prettify the page

--- api/index.js
+++ api/index.js
@@ -1,122 +1,159 @@
-const express = require('express');
-const cors = require('cors');
-const { default: mongoose } = require('mongoose');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const User = require('./models/User.js');
-const Place = require('./models/Place.js');
-const cookieParser = require('cookie-parser');
-const imageDownloader = require('image-downloader');
-const multer = require('multer');
-const fs = require('fs')
+const express = require("express");
+const cors = require("cors");
+const { default: mongoose } = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("./models/User.js");
+const Place = require("./models/Place.js");
+const cookieParser = require("cookie-parser");
+const imageDownloader = require("image-downloader");
+const multer = require("multer");
+const fs = require("fs");
 
-require('dotenv').config();
+require("dotenv").config();
 const app = express();
 
 const bcryptSalt = bcrypt.genSaltSync(10);
-const jwtSecret = 'dsvuh4t8usvbdjb3492udsjbj23408sdv'
+const jwtSecret = "dsvuh4t8usvbdjb3492udsjbj23408sdv";
 
 app.use(express.json());
-app.use('/uploads', express.static(__dirname+'/uploads'))
+app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(cookieParser());
-app.use(cors({
-	credentials: true,
-	origin: 'http://localhost:5173',
-}));
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:5173",
+  })
+);
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL);
 
-app.get('/test', (req, res) => {
-	res.json('test ok');
+app.get("/test", (req, res) => {
+  res.json("test ok");
 });
 
-app.post('/register', async (req, res) => {
-	const {name, email, password} = req.body;
-	try {
-		const userDoc = await User.create({
-			name,
-			email,
-			password: bcrypt.hashSync(password, bcryptSalt)
-		});
-		res.json(userDoc);
-	} catch (e) {
-		res.status(422).json(e);
-	}
-})
-
-app.post('/login', async (req, res) => {
-	const {email,password} = req.body;
-	const userDoc = await User.findOne({email});
-	if (userDoc) {
-		const passOk = bcrypt.compareSync(password, userDoc.password);
-		if (passOk) {
-			jwt.sign({email:userDoc.email, id:userDoc._id}, jwtSecret, {}, (err, token) => {
-				if (err) throw err;
-				res.cookie('token', token).json(userDoc);
-			});
-		} else {
-			res.status(422).json(userDoc);
-		}
-	} else {
-		res.json('Not found')
-	}
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const userDoc = await User.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, bcryptSalt),
+    });
+    res.json(userDoc);
+  } catch (e) {
+    res.status(422).json(e);
+  }
 });
 
-app.get('/profile', (req, res) => {
-	const {token} = req.cookies;
-	if (token) {
-		jwt.verify(token, jwtSecret, {}, async (err, userData) => {
-			if (err) throw err;
-			const {name, email, _id} = await User.findById(userData.id);
-			res.json({name, email, _id});
-		});
-	} else {
-		res.json(null)
-	}
-})
-
-app.post('/logout', (req, res) => {
-	res.cookie('token', '').json(true);
-})
-
-app.post('/upload-by-link', async (req, res) => {
-	const {link} = req.body;
-	const newName = 'photo' + Date.now() + '.jpg';
-	await imageDownloader.image({
-		url: link,
-		dest: __dirname + '/uploads/' + newName,
-	});
-	res.json(newName);
-})
-
-const photosMiddleWare = multer({dest:'uploads/'});
-app.post('/upload', photosMiddleWare.array('photos', 100) , (req, res) => {
-	const uploadedFiles = [];
-	for (let i = 0; i < req.files.length; i++) {
-		const {path, originalname} = req.files[i];
-		const parts = originalname.split('.');
-		const ext = parts[parts.length - 1];
-		const newPath = path + '.' + ext;
-		fs.renameSync(path, newPath);
-		uploadedFiles.push(newPath.replace('uploads/', ''));
-	}
-	res.json(uploadedFiles);
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const userDoc = await User.findOne({ email });
+  if (userDoc) {
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      jwt.sign(
+        { email: userDoc.email, id: userDoc._id },
+        jwtSecret,
+        {},
+        (err, token) => {
+          if (err) throw err;
+          res.cookie("token", token).json(userDoc);
+        }
+      );
+    } else {
+      res.status(422).json(userDoc);
+    }
+  } else {
+    res.json("Not found");
+  }
 });
 
-app.post('/places', (req, res) => {
-	const {token} = req.cookies;
-	const {title, address, addedPhotos, description, perks, 
-		extraInfo, checkIn, checkOut, maxGuests,
-	} = req.body;
-	jwt.verify(token, jwtSecret, {}, async (err, userData) => {
-		if (err) throw err;
-		const placeDoc = await Place.create({
-			owner: userData.id,
-			title, address, addedPhotos, description, perks, 
-			extraInfo, checkIn, checkOut, maxGuests,
-		})
-		res.json(placeDoc);
-	})
-})
+app.get("/profile", (req, res) => {
+  const { token } = req.cookies;
+  if (token) {
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+      if (err) throw err;
+      const { name, email, _id } = await User.findById(userData.id);
+      res.json({ name, email, _id });
+    });
+  } else {
+    res.json(null);
+  }
+});
+
+app.post("/logout", (req, res) => {
+  res.cookie("token", "").json(true);
+});
+
+app.post("/upload-by-link", async (req, res) => {
+  const { link } = req.body;
+  const newName = "photo" + Date.now() + ".jpg";
+  await imageDownloader.image({
+    url: link,
+    dest: __dirname + "/uploads/" + newName,
+  });
+  res.json(newName);
+});
+
+const photosMiddleWare = multer({ dest: "uploads/" });
+app.post("/upload", photosMiddleWare.array("photos", 100), (req, res) => {
+  const uploadedFiles = [];
+  for (let i = 0; i < req.files.length; i++) {
+    const { path, originalname } = req.files[i];
+    const parts = originalname.split(".");
+    const ext = parts[parts.length - 1];
+    const newPath = path + "." + ext;
+    fs.renameSync(path, newPath);
+    uploadedFiles.push(newPath.replace("uploads/", ""));
+  }
+  res.json(uploadedFiles);
+});
+
+app.post("/places", (req, res) => {
+  const { token } = req.cookies;
+  const {
+    title,
+    address,
+    photos,
+    addedPhotos,
+    description,
+    perks,
+    extraInfo,
+    checkIn,
+    checkOut,
+    maxGuests,
+  } = req.body;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    if (err) throw err;
+    const placeDoc = await Place.create({
+      owner: userData.id,
+      title,
+      address,
+      photos,
+      addedPhotos,
+      description,
+      perks,
+      extraInfo,
+      checkIn,
+      checkOut,
+      maxGuests,
+    });
+    res.json(placeDoc);
+  });
+});
+
+app.get("/places", (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    const { id } = userData;
+    res.json(await Place.find({ owner: id }));
+  });
+});
+
+app.get("/places/:id", async (req, res) => {
+  const { id } = req.params;
+  res.json(await Place.findById(id));
+});
 
 app.listen(4000);
