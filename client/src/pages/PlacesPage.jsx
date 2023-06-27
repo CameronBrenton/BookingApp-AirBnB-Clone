a7fa46c0d901e1b1d05134d3a7f9d9ab5bea4469
CameronBrenton <71828277+CameronBrenton@users.noreply.github.com>
2023-06-27 14:40:38 -0230

    Fix the render of the photo thumbnail on the PlacesPage list of places

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -39,9 +39,9 @@ export default function PlacesPage() {
         {places.length > 0 &&
           places.map((place, index) => (
             <Link to={'/account/places/'+place._id} key={index} className="flex gap-4 p-4 bg-gray-100 cursor-pointer rounded-2xl">
-              <div className="w-32 h-32 bg-gray-300 grow shrink-0">
+              <div className="flex w-32 h-32 bg-gray-300 grow shrink-0">
                 {place.photos.length > 0 && (
-                  <img src={place.photos[0]} alt="" />
+                  <img className="object-cover" src={'http://localhost:4000/uploads/' + place.photos[0]} alt="" />
                 )}
               </div>
               <div className="grow-0 shrink">
