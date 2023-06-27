1cb8b37db6e677361378340047d26110d834381f
CameronBrenton <71828277+CameronBrenton@users.noreply.github.com>
2023-06-27 15:32:39 -0230

    Implement a delete photo feature

--- client/src/PhotosUploader.jsx
+++ client/src/PhotosUploader.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 
-export default function PhotosUploader({addedPhotos, onChange}) {
+export default function PhotosUploader({ addedPhotos, onChange }) {
   const [photoLink, setPhotoLink] = useState("");
   async function addPhotoByLink(ev) {
     ev.preventDefault();
@@ -30,6 +30,9 @@ export default function PhotosUploader({addedPhotos, onChange}) {
         });
       });
   }
+  function removePhoto(filename) {
+	onChange([...addedPhotos.filter(photo => photo !== filename)]);
+  }
   return (
     <>
       <div className="flex gap-2">
@@ -49,12 +52,28 @@ export default function PhotosUploader({addedPhotos, onChange}) {
       <div className="grid grid-cols-3 gap-2 mt-2 lg:grid-cols-6 md:grid-cols-4">
         {addedPhotos.length > 0 &&
           addedPhotos.map((link, index) => (
-            <div key={index} className="flex h-32">
+            <div key={index} className="relative flex h-32">
               <img
                 className="object-cover w-full rounded-2xl"
                 src={"http://localhost:4000/uploads/" + link}
                 alt=""
               />
+              <button onClick={() => removePhoto(link)} className="absolute px-3 py-2 text-white bg-black bg-opacity-50 cursor-pointer bottom-1 right-1 rounded-2xl">
+                <svg
+                  width="15"
+                  height="15"
+                  viewBox="0 0 15 15"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M5.5 1C5.22386 1 5 1.22386 5 1.5C5 1.77614 5.22386 2 5.5 2H9.5C9.77614 2 10 1.77614 10 1.5C10 1.22386 9.77614 1 9.5 1H5.5ZM3 3.5C3 3.22386 3.22386 3 3.5 3H5H10H11.5C11.7761 3 12 3.22386 12 3.5C12 3.77614 11.7761 4 11.5 4H11V12C11 12.5523 10.5523 13 10 13H5C4.44772 13 4 12.5523 4 12V4L3.5 4C3.22386 4 3 3.77614 3 3.5ZM5 4H10V12H5V4Z"
+                    fill="currentColor"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </button>
             </div>
           ))}
         <label className="flex items-center justify-center h-32 p-2 text-2xl text-gray-600 bg-transparent border cursor-pointer rounded-2xl">
