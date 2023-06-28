de6ff5ad69ffa90003ee8fe273676d5f1303a6f7
CameronBrenton <71828277+CameronBrenton@users.noreply.github.com>
2023-06-28 11:27:42 -0230

    Style the index page grid of places

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -9,19 +9,24 @@ export default function IndexPage() {
     });
   }, []);
   return (
-    <div className="grid mt-8 grid-cold-2 md:grid-col-3 lg:grid-col-4">
+    <div className="grid grid-cols-2 mt-8 gap-y-8 gap-x-6 md:grid-cols-3 lg:grid-cols-4">
       {places.length > 0 &&
         places.map((place) => (
           <div>
-            <div className="bg-gray-500">
+            <div className="flex mb-2 bg-gray-500 rounded-2xl">
               {place.photos?.[0] && (
                 <img
+				  className="object-cover rounded-2xl aspect-square"
                   src={"http://localhost:4000/uploads/" + place.photos?.[0]}
                   alt=""
                 />
               )}
             </div>
-            {place.title}
+			<h3 className="font-bold">{place.address}</h3>
+			<h2 className="text-sm text-gray-500">{place.title}</h2>
+			<div className="mt-1">
+				<span className="font-bold">${place.price}</span> per night
+			</div>
           </div>
         ))}
     </div>
