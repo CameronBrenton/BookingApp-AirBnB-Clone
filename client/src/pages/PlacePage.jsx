bce381cd3e5ac370f070131906c161fec0773eb1
CameronBrenton <71828277+CameronBrenton@users.noreply.github.com>
2023-06-28 12:41:43 -0230

    Add styling to the PlacePage so the images are laid out like AirBnB

--- client/src/pages/PlacePage.jsx
+++ client/src/pages/PlacePage.jsx
@@ -21,6 +21,25 @@ export default function PlacePage() {
     <div className="px-8 py-8 mt-4 -mx-8 bg-gray-100">
       <h1 className="text-3xl">{place.title}</h1>
       <a className='block my-2 font-semibold underline' target='_blank' href={'https://maps.google.com/?q='+place.address} rel="noreferrer">{place.address}</a>
+	  <div className="grid gap-2 grid-cols-[2fr_1fr]">
+		<div className="">
+			{place.photos?.[0] && (
+				<div className="">
+					<img className='object-cover aspect-square' src={'http://localhost:4000/uploads/'+place.photos[0]} alt="" />
+				</div>
+			)}
+		</div>
+		<div className="grid ">
+		{place.photos?.[1] && (
+				<img className='object-cover aspect-square' src={'http://localhost:4000/uploads/'+place.photos[1]} alt="" />
+			)}
+			<div className="overflow-hidden">
+			{place.photos?.[2] && (
+				<img className='relative object-cover aspect-square top-2' src={'http://localhost:4000/uploads/'+place.photos[2]} alt="" />
+			)}
+			</div>
+		</div>
+	  </div>
     </div>
   );
 }
