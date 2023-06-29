166472b6116b56550186e4c90e250d796a332dd5
CameronBrenton <71828277+CameronBrenton@users.noreply.github.com>
2023-06-29 21:08:45 -0230

    Add state for the inputs

--- client/src/BookingWidget.jsx
+++ client/src/BookingWidget.jsx
@@ -1,4 +1,9 @@
-export default function BookingWidget({place}) {
+import { useState } from "react";
+
+export default function BookingWidget({ place }) {
+  const [checkIn, setCheckIn] = useState("");
+  const [checkOut, setCheckOut] = useState("");
+  const [numberOfGuests, setNumberOfGuests] = useState(1);
   return (
     <div className="p-4 bg-white shadow rounded-2xl">
       <div className="text-2xl text-center">
@@ -8,16 +13,31 @@ export default function BookingWidget({place}) {
         <div className="flex">
           <div className="px-4 py-3">
             <label htmlFor="">Check in:</label>
-            <input type="date" className="" />
+            <input
+              type="date"
+              className=""
+              value={checkIn}
+              onChange={(ev) => setCheckIn(ev.target.value)}
+            />
           </div>
           <div className="px-4 py-3 border-l">
             <label htmlFor="">Check out:</label>
-            <input type="date" className="" />
+            <input
+              type="date"
+              className=""
+              value={checkOut}
+              onChange={(ev) => setCheckOut(ev.target.value)}
+            />
           </div>
         </div>
         <div className="px-4 py-3 border-t">
           <label htmlFor="">Number of guests:</label>
-          <input type="number" className="" value={1} />
+          <input
+            type="number"
+            className=""
+            value={numberOfGuests}
+            onChange={(ev) => setNumberOfGuests(ev.target.value)}
+          />
         </div>
       </div>
       <button className="mt-4 primary"> Book this place</button>
