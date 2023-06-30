2cb6115fc684ad0235d42dcb6d8b37574116e295
CameronBrenton <71828277+CameronBrenton@users.noreply.github.com>
2023-06-30 13:27:41 -0230

    Update BookingsPage
    -Display booking times on the page

--- client/src/pages/BookingsPage.jsx
+++ client/src/pages/BookingsPage.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
+import AccountNav from "../AccountNav";
+import axios from "axios";
+
 export default function BookingsPage() {
-	return (
-		<div className="">
-			my bookings here
-		</div>
-	);
+  const [bookings, setBookings] = useState([]);
+  useEffect(() => {
+    axios.get("/bookings").then((response) => {
+      setBookings(response.data);
+    });
+  }, []);
+  return (
+    <div className="">
+      <AccountNav />
+      <div className="">
+        {bookings?.length > 0 &&
+          bookings.map((booking) => (
+            <div>
+              {booking.checkIn} -> {booking.checkOut}
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 }
