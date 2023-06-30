981301f23494db6dfa986484ffefcd5ef75e08da
CameronBrenton <71828277+CameronBrenton@users.noreply.github.com>
2023-06-30 16:00:29 -0230

    Trim PlacePage code to Create PlaceGallery component

---
+++ client/src/PlaceGallery.jsx
@@ -0,0 +1,108 @@
+import { useState } from "react";
+
+export default function PlaceGallery({ place }) {
+  const [showAllPhotos, setShowAllPhotos] = useState(false);
+
+  if (showAllPhotos) {
+    return (
+      <div className="absolute inset-0 min-h-screen text-white bg-black">
+        <div className="grid gap-4 p-8 bg-black ">
+          <div className="">
+            <h2 className="mr-48 text-3xl">Photos of {place.title}</h2>
+            <button
+              onClick={() => setShowAllPhotos(false)}
+              className="fixed flex items-center gap-1 px-4 py-2 text-black bg-white shadow right-12 top-8 shadow-black rounded-2xl"
+            >
+              <svg
+                width="15"
+                height="15"
+                viewBox="0 0 15 15"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M12.8536 2.85355C13.0488 2.65829 13.0488 2.34171 12.8536 2.14645C12.6583 1.95118 12.3417 1.95118 12.1464 2.14645L7.5 6.79289L2.85355 2.14645C2.65829 1.95118 2.34171 1.95118 2.14645 2.14645C1.95118 2.34171 1.95118 2.65829 2.14645 2.85355L6.79289 7.5L2.14645 12.1464C1.95118 12.3417 1.95118 12.6583 2.14645 12.8536C2.34171 13.0488 2.65829 13.0488 2.85355 12.8536L7.5 8.20711L12.1464 12.8536C12.3417 13.0488 12.6583 13.0488 12.8536 12.8536C13.0488 12.6583 13.0488 12.3417 12.8536 12.1464L8.20711 7.5L12.8536 2.85355Z"
+                  fill="currentColor"
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+              Close photos
+            </button>
+          </div>
+          {place?.photos?.length > 0 &&
+            place.photos.map((photo) => (
+              <div className="">
+                <img src={"http://localhost:4000/uploads/" + photo} alt="" />
+              </div>
+            ))}
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="relative">
+      <div className="grid gap-2 grid-cols-[2fr_1fr] rounded-3xl overflow-hidden">
+        <div className="">
+          {place.photos?.[0] && (
+            <div className="">
+              <img
+                onClick={() => {
+                  setShowAllPhotos(true);
+                }}
+                className="object-cover cursor-pointer aspect-square"
+                src={"http://localhost:4000/uploads/" + place.photos[0]}
+                alt=""
+              />
+            </div>
+          )}
+        </div>
+        <div className="grid ">
+          {place.photos?.[1] && (
+            <img
+              onClick={() => {
+                setShowAllPhotos(true);
+              }}
+              className="object-cover cursor-pointer aspect-square"
+              src={"http://localhost:4000/uploads/" + place.photos[1]}
+              alt=""
+            />
+          )}
+          <div className="overflow-hidden">
+            {place.photos?.[2] && (
+              <img
+                onClick={() => {
+                  setShowAllPhotos(true);
+                }}
+                className="relative object-cover cursor-pointer aspect-square top-2"
+                src={"http://localhost:4000/uploads/" + place.photos[2]}
+                alt=""
+              />
+            )}
+          </div>
+        </div>
+      </div>
+      <button
+        onClick={() => setShowAllPhotos(true)}
+        className="absolute flex items-center gap-1 px-4 py-2 bg-white shadow-md shadow-black bottom-2 right-2 rounded-2xl"
+      >
+        <svg
+          width="15"
+          height="15"
+          viewBox="0 0 15 15"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M2.5 1H12.5C13.3284 1 14 1.67157 14 2.5V12.5C14 13.3284 13.3284 14 12.5 14H2.5C1.67157 14 1 13.3284 1 12.5V2.5C1 1.67157 1.67157 1 2.5 1ZM2.5 2C2.22386 2 2 2.22386 2 2.5V8.3636L3.6818 6.6818C3.76809 6.59551 3.88572 6.54797 4.00774 6.55007C4.12975 6.55216 4.24568 6.60372 4.32895 6.69293L7.87355 10.4901L10.6818 7.6818C10.8575 7.50607 11.1425 7.50607 11.3182 7.6818L13 9.3636V2.5C13 2.22386 12.7761 2 12.5 2H2.5ZM2 12.5V9.6364L3.98887 7.64753L7.5311 11.4421L8.94113 13H2.5C2.22386 13 2 12.7761 2 12.5ZM12.5 13H10.155L8.48336 11.153L11 8.6364L13 10.6364V12.5C13 12.7761 12.7761 13 12.5 13ZM6.64922 5.5C6.64922 5.03013 7.03013 4.64922 7.5 4.64922C7.96987 4.64922 8.35078 5.03013 8.35078 5.5C8.35078 5.96987 7.96987 6.35078 7.5 6.35078C7.03013 6.35078 6.64922 5.96987 6.64922 5.5ZM7.5 3.74922C6.53307 3.74922 5.74922 4.53307 5.74922 5.5C5.74922 6.46693 6.53307 7.25078 7.5 7.25078C8.46693 7.25078 9.25078 6.46693 9.25078 5.5C9.25078 4.53307 8.46693 3.74922 7.5 3.74922Z"
+            fill="currentColor"
+            fillRule="evenodd"
+            clipRule="evenodd"
+          ></path>
+        </svg>
+        Show more photos
+      </button>
+    </div>
+  );
+}
