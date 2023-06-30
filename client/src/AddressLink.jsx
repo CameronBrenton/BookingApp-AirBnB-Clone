eaf56f96e33a5f79e8efa62b731f628e1ec9b0c7
CameronBrenton <71828277+CameronBrenton@users.noreply.github.com>
2023-06-30 16:51:01 -0230

    Split PlacePage code into AddressLink component

---
+++ client/src/AddressLink.jsx
@@ -0,0 +1,35 @@
+export default function AddressLink({children, className=null}) {
+	if (!className) {
+		className = 'block my-3'
+	}
+	className += ' flex font-semibold gap-1 underline'
+	return (
+		<a
+        className={className}
+        target="_blank"
+        href={"https://maps.google.com/?q=" + children}
+        rel="noreferrer"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+          />
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+          />
+        </svg>
+        {children}
+      </a>
+	);
+}
--- client/src/pages/PlacePage.jsx
+++ client/src/pages/PlacePage.jsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import BookingWidget from "../BookingWidget";
 import PlaceGallery from "../PlaceGallery";
+import AddressLink from "../AddressLink";
 
 export default function PlacePage() {
   const { id } = useParams();
@@ -24,35 +25,9 @@ export default function PlacePage() {
   
   return (
     <div className="px-8 pt-8 mt-4 -mx-8 bg-gray-100">
-      <h1 className="text-3xl">{place.title}</h1>
-      <a
-        className="flex gap-1 my-3 font-semibold underline"
-        target="_blank"
-        href={"https://maps.google.com/?q=" + place.address}
-        rel="noreferrer"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-6 h-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
-          />
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
-          />
-        </svg>
-
-        {place.address}
-      </a>
+	<AddressLink>
+		{place.address}
+	</AddressLink>
       <PlaceGallery place={place} />
       <div className="mt-8 mb-8 gap-4 grid grid-cols-1 md:grid-cols-[2fr_1fr]">
         <div className="">
