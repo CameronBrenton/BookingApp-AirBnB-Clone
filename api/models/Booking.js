dc0a615a20bbc1f59f04377b7e54bb6aaefa7ab6
CameronBrenton <71828277+CameronBrenton@users.noreply.github.com>
2023-06-30 12:35:02 -0230

    Add user to Booking model

--- api/models/Booking.js
+++ api/models/Booking.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const bookingSchema = new mongoose.Schema({
 	place: {type:mongoose.Schema.Types.ObjectId, required:true},
+	user: {type:mongoose.Schema.Types.ObjectId, required:true},
 	checkIn: {type:Date, required:true},
 	checkOut: {type:Date, required:true},
 	name: {type:String, required:true},
