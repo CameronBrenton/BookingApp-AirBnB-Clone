a41f2aecfb57e31fc0f32e2e14df354ad3a4da7b
CameronBrenton <71828277+CameronBrenton@users.noreply.github.com>
2023-06-30 15:08:47 -0230

    Update the schema to reference place object

--- api/models/Booking.js
+++ api/models/Booking.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const bookingSchema = new mongoose.Schema({
-	place: {type:mongoose.Schema.Types.ObjectId, required:true},
+	place: {type:mongoose.Schema.Types.ObjectId, required:true, ref: 'Place'},
 	user: {type:mongoose.Schema.Types.ObjectId, required:true},
 	checkIn: {type:Date, required:true},
 	checkOut: {type:Date, required:true},
